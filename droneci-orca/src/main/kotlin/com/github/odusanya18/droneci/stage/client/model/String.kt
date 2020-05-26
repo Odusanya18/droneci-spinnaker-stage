b67b8532c45b4ef47e9e16d88dc526813a15abf9
Victor Odusanya <victor.odusanya@interswitchgroup.com>
2020-05-26 12:36:32 +0100

    added list namespace, repo on drone con for deck

--- droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/stage/client/model/String.kt
+++
@@ -1,8 +0,0 @@
-package com.github.odusanya18.droneci.stage.client.model
-import java.time.ZonedDateTime
-import kotlin.String
-
-data class String (
-    val accessToken: String,
-    val expiry: ZonedDateTime
-)
--- droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/stage/controllers/DroneCIController.kt
+++ droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/stage/controllers/DroneCIController.kt
@@ -1,14 +1,36 @@
 package com.github.odusanya18.droneci.stage.controllers
 
+import com.github.odusanya18.droneci.stage.client.DroneCIClient
+import com.github.odusanya18.droneci.stage.config.DroneCIProperties
+import com.netflix.spinnaker.kork.plugins.api.PluginSdks
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController()
 @RequestMapping("/drone-ci")
-class DroneCIController {
+class DroneCIController(pluginSdks: PluginSdks) {
+    private val droneCIClient: DroneCIClient
+    init {
+        val droneCIProperties = pluginSdks
+            .yamlResourceLoader()
+            .loadResource("drone-ci.yml", DroneCIProperties::class.java)
+        droneCIClient = DroneCIClient(droneCIProperties.baseUrl, droneCIProperties.token, droneCIProperties.refresh)
+    }
 
-    @GetMapping("/repos")
-    fun repos() = ""
+    @GetMapping("/namespaces")
+    fun namespaces() = droneCIClient
+        .repoService
+        .listRepos()
+        .distinctBy { repo -> repo.namespace }
+        .map { repo -> repo.namespace }
+
+    @GetMapping("/repos/{namespace}")
+    fun reposByNamespace(@PathVariable("namespace") namespace: String) = droneCIClient
+        .repoService
+        .listRepos()
+        .filter { repo -> repo.namespace == namespace }
 }
