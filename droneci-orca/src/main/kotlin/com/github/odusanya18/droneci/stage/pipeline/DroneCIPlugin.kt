cc8c635dbf06840057bd6803ac0329292fe10093
Victor Odusanya <victor.odusanya@interswitchgroup.com>
2020-05-26 15:25:06 +0100

    combine droneci stage and droneciplugin

--- droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/stage/pipeline/DroneCIPlugin.kt
+++ droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/stage/pipeline/DroneCIPlugin.kt
@@ -1,6 +1,5 @@
 package com.github.odusanya18.droneci.stage.pipeline
 
-import com.github.odusanya18.droneci.stage.models.definition.CIStageDefinition
 import com.github.odusanya18.droneci.stage.controller.DroneCIController
 import com.github.odusanya18.droneci.stage.tasks.MonitorDroneCITask
 import com.github.odusanya18.droneci.stage.tasks.StartDroneCITask
@@ -8,23 +7,20 @@ import com.netflix.spinnaker.orca.api.pipeline.graph.TaskNode.Builder
 import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution
 import com.netflix.spinnaker.orca.api.pipeline.graph.StageDefinitionBuilder
 import com.netflix.spinnaker.kork.plugins.api.spring.PrivilegedSpringPlugin
-import com.netflix.spinnaker.orca.api.pipeline.CancellableStage
 import org.pf4j.Extension
 import org.pf4j.PluginWrapper
 import org.springframework.beans.factory.support.BeanDefinitionRegistry
 
-class DroneCIPlugin(wrapper: PluginWrapper) : PrivilegedSpringPlugin(wrapper) {
+@Extension
+class DroneCIPlugin(wrapper: PluginWrapper) : PrivilegedSpringPlugin(wrapper), StageDefinitionBuilder {
     override fun registerBeanDefinitions(registry: BeanDefinitionRegistry) {
         registerBean(beanDefinitionFor(DroneCIController::class.java), registry)
     }
 
-    @Extension
-    class DroneCIStage : StageDefinitionBuilder {
-        override fun taskGraph(stage: StageExecution, builder: Builder) {
-            builder
-                .withTask("startDroneCITask", StartDroneCITask::class.java)
-                .withTask("monitorDroneCITask", MonitorDroneCITask::class.java)
-                .withTask("stopDroneCITask", MonitorDroneCITask::class.java)
-        }
+    override fun taskGraph(stage: StageExecution, builder: Builder) {
+        builder
+            .withTask("startDroneCITask", StartDroneCITask::class.java)
+            .withTask("monitorDroneCITask", MonitorDroneCITask::class.java)
+            .withTask("stopDroneCITask", MonitorDroneCITask::class.java)
     }
 }
