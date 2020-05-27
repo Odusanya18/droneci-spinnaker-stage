5ff46c4dc059e005a9fc007a9d93927dee621519
Victor Odusanya <victor.odusanya@interswitchgroup.com>
2020-05-27 12:57:14 +0100

    delomboked ci props

--- droneci-orca/src/main/java/com/github/odusanya18/droneci/stage/config/DroneCIProperties.java
+++ droneci-orca/src/main/java/com/github/odusanya18/droneci/stage/config/DroneCIProperties.java
@@ -2,16 +2,38 @@ package com.github.odusanya18.droneci.stage.config;
 
 import com.github.odusanya18.droneci.stage.models.Master;
 import com.netflix.spinnaker.kork.plugins.api.ExtensionConfiguration;
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
 
 @ExtensionConfiguration("odusanya18.drone-ci")
-@ConfigurationProperties("odusanya18.drone-ci")
-@Data
+@ConfigurationProperties("spinnaker.extensibility.plugins.odusanya18.drone-ci")
 public class DroneCIProperties {
-    public Map<String, Master> masters;
-    public Long timeout;
-    public Long backOffPeriod;
+    private Map<String, Master> masters;
+    private Long timeout = 300L;
+    private Long backOffPeriod = 500L;
+
+    public Map<String, Master> getMasters() {
+        return masters;
+    }
+
+    public void setMasters(Map<String, Master> masters) {
+        this.masters = masters;
+    }
+
+    public Long getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(Long timeout) {
+        this.timeout = timeout;
+    }
+
+    public Long getBackOffPeriod() {
+        return backOffPeriod;
+    }
+
+    public void setBackOffPeriod(Long backOffPeriod) {
+        this.backOffPeriod = backOffPeriod;
+    }
 }
