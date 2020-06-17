e43137e639bfc8e37fda676cbaa2a88e9827818e
Victor Odusanya <>
2020-06-17 11:24:29 -0500

    (droneci-orca) log ctx

--- droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/orca/util/TaskUtil.kt
+++ droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/orca/util/TaskUtil.kt
@@ -1,7 +1,10 @@
 package com.github.odusanya18.droneci.orca.util
 
+import com.google.gson.Gson
 import com.netflix.spinnaker.orca.api.pipeline.TaskResult
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 object TaskUtil {
     fun task(status: String, buildNumber: Long?) = mapOf(
@@ -16,6 +19,9 @@ object TaskUtil {
     }
 
     fun buildNumber(ctx: Map<String, Any>): Int {
+        val logger = LoggerFactory.getLogger(this.javaClass)
+        val gson = Gson()
+        logger.info(gson.toJson(ctx))
         val buildInfo = ctx["buildInfo"] as Map<*, *>
         return buildInfo["number"] as Int
     }
