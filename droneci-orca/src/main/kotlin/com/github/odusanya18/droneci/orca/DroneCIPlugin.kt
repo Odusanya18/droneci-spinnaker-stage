4c66dbc8036e4f10df5adf170fbb66d8f09b6870
Victor Odusanya <>
2020-06-17 08:36:27 -0500

    (droneci-orca) implement cancellable stage

--- droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/orca/DroneCIPlugin.kt
+++ droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/orca/DroneCIPlugin.kt
@@ -1,22 +1,30 @@
 package com.github.odusanya18.droneci.orca
 
 import com.github.odusanya18.droneci.orca.tasks.*
+import com.netflix.spinnaker.orca.api.pipeline.CancellableStage
+import com.netflix.spinnaker.orca.api.pipeline.CancellableStage.Result
+import com.netflix.spinnaker.orca.api.pipeline.graph.StageDefinitionBuilder
 import com.netflix.spinnaker.orca.api.pipeline.graph.TaskNode.Builder
 import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution
-import com.netflix.spinnaker.orca.api.pipeline.graph.StageDefinitionBuilder
 import org.pf4j.Extension
 import org.pf4j.Plugin
 import org.pf4j.PluginWrapper
+import java.util.*
 
 class DroneCIPlugin(wrapper: PluginWrapper) : Plugin(wrapper)
 
 @Extension
-class DroneCIStage : StageDefinitionBuilder {
+class DroneCIStage(private val stopDroneCITask: StopDroneCITask) : StageDefinitionBuilder, CancellableStage {
     override fun taskGraph(stage: StageExecution, builder: Builder) {
         builder
             .withTask("startDroneCITask", StartDroneCITask::class.java)
             .withTask("monitorDroneCITask", MonitorDroneCITask::class.java)
-            .withTask("stopDroneCITask", StopDroneCITask::class.java)
+    }
+
+    override fun cancel(stage: StageExecution): Result {
+        val result = stopDroneCITask.execute(stage)
+        stage.context = result.context
+        return Result(stage, stage.context)
     }
 }
 
