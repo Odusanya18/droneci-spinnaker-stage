785e1d8342df78983adf9d423807007a222dc20a
Victor Odusanya <>
2020-06-17 09:42:42 -0500

    (droneci-gate) document endpoints

--- droneci-gate/src/main/kotlin/com/github/odusanya18/droneci/gate/controller/DroneCIController.kt
+++ droneci-gate/src/main/kotlin/com/github/odusanya18/droneci/gate/controller/DroneCIController.kt
@@ -1,6 +1,7 @@
 package com.github.odusanya18.droneci.gate.controller
 
 import com.github.odusanya18.droneci.client.internal.ServiceClient
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import java.io.IOException
@@ -8,6 +9,7 @@ import java.io.IOException
 @RestController
 @RequestMapping("/drone-ci/masters")
 class DroneCIController(private val serviceClient: ServiceClient) {
+    @ApiOperation(value = "Retrieve a list of Drone Masters", response = List::class)
     @GetMapping
     private fun masters() : List<String> {
         val masters = serviceClient
@@ -22,6 +24,7 @@ class DroneCIController(private val serviceClient: ServiceClient) {
         throw DroneIgorException(masters.message())
     }
 
+    @ApiOperation(value = "Retrieve a list of Drone Namespaces by Master", response = List::class)
     @GetMapping("/{master}/namespaces")
     fun namespaces(@PathVariable("master") master: String): List<String> {
         val namespaces = serviceClient
@@ -36,6 +39,7 @@ class DroneCIController(private val serviceClient: ServiceClient) {
         throw DroneIgorException(namespaces.message())
     }
 
+    @ApiOperation(value = "Retrieve a list of Drone Repos by Namespace", response = List::class)
     @GetMapping("/{master}/namespaces/{namespace}/repos")
     fun reposByNamespace(@PathVariable("master") master: String, @PathVariable("namespace") namespace: String): List<String> {
         val repos = serviceClient
