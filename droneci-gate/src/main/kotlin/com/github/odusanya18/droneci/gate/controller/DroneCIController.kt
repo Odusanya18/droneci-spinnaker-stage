be6d7f7fb87956a3eebca52add61b31a5a38fdc9
Victor Odusanya <>
2020-06-17 09:27:31 -0500

    (droneci-gate) error handling on igor calls

--- droneci-gate/src/main/kotlin/com/github/odusanya18/droneci/gate/controller/DroneCIController.kt
+++ droneci-gate/src/main/kotlin/com/github/odusanya18/droneci/gate/controller/DroneCIController.kt
@@ -1,33 +1,55 @@
 package com.github.odusanya18.droneci.gate.controller
 
 import com.github.odusanya18.droneci.client.internal.ServiceClient
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import java.io.IOException
 
 @RestController
 @RequestMapping("/drone-ci/masters")
 class DroneCIController(private val serviceClient: ServiceClient) {
     @GetMapping
-    private fun masters() = serviceClient
-        .igorService
-        .getMasters()
-        .execute()
-        .body()
+    private fun masters() : List<String> {
+        val masters = serviceClient
+            .igorService
+            .getMasters()
+            .execute()
+        if (masters.isSuccessful) {
+            return masters
+                .body()
+                .orEmpty()
+        }
+        throw DroneIgorException(masters.message())
+    }
 
     @GetMapping("/{master}/namespaces")
-    fun namespaces(@PathVariable("master") master: String) = serviceClient
-        .igorService
-        .getNamespacesByMaster(master)
-        .execute()
-        .body()
+    fun namespaces(@PathVariable("master") master: String): List<String> {
+        val namespaces = serviceClient
+            .igorService
+            .getNamespacesByMaster(master)
+            .execute()
+        if (namespaces.isSuccessful){
+            return namespaces
+                .body()
+                .orEmpty()
+        }
+        throw DroneIgorException(namespaces.message())
+    }
 
     @GetMapping("/{master}/namespaces/{namespace}/repos")
-    fun reposByNamespace(@PathVariable("master") master: String, @PathVariable("namespace") namespace: String) =
-        serviceClient
+    fun reposByNamespace(@PathVariable("master") master: String, @PathVariable("namespace") namespace: String): List<String> {
+        val repos = serviceClient
             .igorService
             .getReposByNamespace(master, namespace)
             .execute()
-            .body()
+        if (repos.isSuccessful){
+            return repos
+                .body()
+                .orEmpty()
+        }
+        throw DroneIgorException(repos.message())
+    }
+
+    @ResponseStatus(code = HttpStatus.SERVICE_UNAVAILABLE, reason = "Request to igor failed")
+    private class DroneIgorException internal constructor(message: String) : IOException(message)
 }
