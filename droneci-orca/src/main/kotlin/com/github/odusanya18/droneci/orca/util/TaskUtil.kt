6c337d7421bc36b77cde216ff505ea2452bbde26
Victor Odusanya <>
2020-06-16 19:22:45 -0500

    (droneci-orca) convert buildInfo to Map

--- droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/orca/util/TaskUtil.kt
+++ droneci-orca/src/main/kotlin/com/github/odusanya18/droneci/orca/util/TaskUtil.kt
@@ -1,5 +1,7 @@
 package com.github.odusanya18.droneci.orca.util
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.netflix.spinnaker.orca.api.pipeline.TaskResult
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus
 
@@ -7,11 +9,21 @@ object TaskUtil {
     fun task(status: String, buildNumber: Long?) = mapOf(
         "action" to status,
         "number" to buildNumber.toString())
+
+    private inline fun <I, reified O> I.convert(): O {
+        val gson = Gson()
+        val json = gson.toJson(this)
+        return gson.fromJson(json, object : TypeToken<O>() {}.type)
+    }
+
+    private fun <T> T.serializeToMap(): Map<String, Any> {
+        return convert()
+    }
+
     fun taskResult(executionStatus: ExecutionStatus, buildInfo: Any): TaskResult {
-        @Suppress("UNCHECKED_CAST")
         return TaskResult
             .builder(executionStatus)
-            .context(buildInfo as MutableMap<out String, *>?)
+            .context(buildInfo.serializeToMap())
             .build()
     }
 }
