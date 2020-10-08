c60660b1fbf09779edc4498bef9b05a56a9aa5b1
Armando Magalhães <amagalhaes@netflix.com>
2020-10-08 19:55:19 +0200

    style: setup prettier rules and editorconfig

--- droneci-deck/src/drone-ci/droneCIStage.ts
+++ droneci-deck/src/drone-ci/droneCIStage.ts
@@ -1,20 +1,32 @@
-import { IStageTypeConfig } from "@spinnaker/core";
-import { DroneCIExecutionDetails } from "./DroneCIExecutionDetails";
-import { DroneCIStageConfig } from "./DroneCIStageConfig";
+import { IStageTypeConfig } from '@spinnaker/core';
+import { DroneCIExecutionDetails } from './DroneCIExecutionDetails';
+import { DroneCIStageConfig } from './DroneCIStageConfig';
 
 export const droneCIStage: IStageTypeConfig = {
-    label: 'Drone CI Build',
-    description: 'Runs a Drone CI Build',
-    key: 'droneCI',
-    defaults: {
-        failOnFailedExpressions: true,
+  label: 'Drone CI Build',
+  description: 'Runs a Drone CI Build',
+  key: 'droneCI',
+  defaults: {
+    failOnFailedExpressions: true,
+  },
+  component: DroneCIStageConfig,
+  executionDetailsSections: [DroneCIExecutionDetails],
+  strategy: true,
+  validators: [
+    {
+      type: 'requiredField',
+      fieldName: 'master',
+      fieldLabel: 'Build Master',
     },
-    component: DroneCIStageConfig,
-    executionDetailsSections: [DroneCIExecutionDetails],
-    strategy: true,
-    validators: [
-        { type: 'requiredField', fieldName: 'master', fieldLabel: 'Build Master' },
-        { type: 'requiredField', fieldName: 'namespace', fieldLabel: 'Build Namespace' },
-        { type: 'requiredField', fieldName: 'repo', fieldLabel: 'Repository' },
-    ],
-}
+    {
+      type: 'requiredField',
+      fieldName: 'namespace',
+      fieldLabel: 'Build Namespace',
+    },
+    {
+      type: 'requiredField',
+      fieldName: 'repo',
+      fieldLabel: 'Repository',
+    },
+  ],
+};
